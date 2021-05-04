b0301bb887be1bf39d3d87991a9c30118cf2012e
락원 <geekseong@gmail.com>
2021-05-04 20:26:52 +0900

    refactor(Input): 숫자 입력 유효성 검사 실패시 문자열을 하드코딩이 아닌 상수로 변경.

--- src/main/java/game/io/Input.java
+++ src/main/java/game/io/Input.java
@@ -6,6 +6,7 @@ import game.utils.ValidationUtil;
 
 public class Input {
 	private final static Scanner INPUT = new Scanner(System.in);
+	public static final String REQUIRED_ONLY_NUMBER_MSG = "숫자만 입력이 가능합니다.";
 
 	public static String readString() {
 		return INPUT.nextLine();
@@ -14,7 +15,7 @@ public class Input {
 	public static int readInt() {
 		String line = readString();
 		while (!ValidationUtil.isNumber(line)) {
-			Output.println("숫자만 입력이 가능합니다.");
+			Output.println(REQUIRED_ONLY_NUMBER_MSG);
 			line = readString();
 		}
 
